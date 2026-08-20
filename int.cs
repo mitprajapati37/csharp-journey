aa3274c13da537896778dc3e93371b5cc2a0ed21
mit <pmeet2500@gmail.com>
2026-08-20 17:14:34 +0530

    integer Datatyes

---
+++ int.cs
@@ -0,0 +1,14 @@
+int a = 10;
+Console.WriteLine(a);            // this first one is currect syntex for int       
+
+//int a = "mit prajapati";              //  Cannot implicitly convert type 'string' to 'int'
+//Console.WriteLine(a);           
+
+//int a = 10.5;                  // Cannot implicitly convert type 'double' to 'int'. An explicit conversion exists (are you missing a cast?)
+//Console.WriteLine(a);        // I learn Base on Error Default Floating point is double.
+
+
+//int a = 10.5f;                 // Cannot implicitly convert type 'float' to 'int'. An explicit conversion exists (are you missing a cast?)
+//Console.WriteLine(a);          //Base on Error Written "F" is a Float.
+
+
