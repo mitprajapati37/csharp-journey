64bb4e2a6026440f4d9c1e707c7829eddd7db093
mitprajapati37 <pmeet2500@gmail.com>
2026-08-21 19:13:33 +0530

    Add numeric types in C#
    This file demonstrates the usage of various numeric types in C# including sbyte, byte, ushort, short, int, uint, long, ulong, double, decimal, and float. It prints their values along with their respective min and max values.

---
+++ Numbers.cs
@@ -0,0 +1,66 @@
+sbyte m = 123;
+Console.WriteLine("-----------------sbyte----------------");
+Console.WriteLine(m);
+Console.WriteLine(sbyte.MaxValue);
+Console.WriteLine(sbyte.MinValue);
+
+
+byte n = 255;
+Console.WriteLine("---------------byte----------------");
+Console.WriteLine(n);
+Console.WriteLine(byte.MinValue);
+Console.WriteLine(byte.MaxValue);
+
+ushort u = 65375;
+Console.WriteLine("-----------------unshort---------------");
+Console.WriteLine(u);
+Console.WriteLine(ushort.MinValue);
+Console.WriteLine(ushort.MaxValue);
+
+short s = 32000;
+Console.WriteLine("------------------short-------------------");
+Console.WriteLine(s);
+Console.WriteLine(short.MinValue);
+Console.WriteLine(short.MaxValue);
+
+int a = 10;
+Console.WriteLine("--------------Int-------------------");
+Console.WriteLine(int.MaxValue);
+Console.WriteLine(int.MinValue);
+
+uint ui = 265568545;
+Console.WriteLine("-----------------------uint------------------");
+Console.WriteLine(ui);
+Console.WriteLine(byte.MinValue);
+Console.WriteLine(byte.MaxValue);
+
+
+long b = 154564654654654L;
+Console.WriteLine("------------------Long-------------------------");
+Console.WriteLine(b);
+Console.WriteLine(long.MaxValue);
+Console.WriteLine(long.MinValue);
+
+ulong ul = 251546516511566;
+Console.WriteLine("----------------------ulong----------------------");
+Console.WriteLine(ul);
+Console.WriteLine(ulong.MaxValue);
+Console.WriteLine(ulong.MinValue);
+
+double d = 6545132654.5D;
+Console.WriteLine("-----------------------double------------------------");
+Console.WriteLine(d);
+Console.WriteLine(double.MaxValue);
+Console.WriteLine(double.MinValue);
+
+decimal df = 100455.55652M;
+Console.WriteLine("-----------------------decimal---------------------------");
+Console.WriteLine(df);
+Console.WriteLine(decimal.MaxValue);
+Console.WriteLine(decimal.MinValue);
+
+float f = 25.256254F;
+Console.WriteLine("-------------------------float--------------------------------------");
+Console.WriteLine(f);
+Console.WriteLine(Single.MinValue);
+Console.WriteLine(Single.MaxValue);
