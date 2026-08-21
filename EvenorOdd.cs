55e95f2ba3b8e93f56c2c613b3effae3ae321cf3
mitprajapati37 <pmeet2500@gmail.com>
2026-08-21 19:21:23 +0530

    Add Even or Odd checker for a single number

---
+++ EvenorOdd.cs
@@ -0,0 +1,14 @@
+Console.Write("Enter the number1:-");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+//Console.Write("Enter the number:-");
+//int n2 = Convert.ToInt32(Console.ReadLine());
+
+if(n1 % 2 == 0)
+{
+    Console.WriteLine($"Is Even {n1}");
+}
+else
+{
+    Console.WriteLine($"is Odd {n1}");
+}
